4dfdfa2dc7bce6c2fe6fdaa6f725929e68ea98b5
WaynecarlG <wayneoriginal01@outlook.com>
2025-10-27 06:51:47 +0300

    basics and styling methods

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -6,6 +8,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <div
+  className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+/>
     </html>
   );
 }
